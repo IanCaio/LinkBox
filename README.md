0dee5dbd9ab327cca473e8b6afa8b8c1c74423ed
IanCaio <iancaio_dev@hotmail.com>
2018-01-03 23:30:01 -0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# LinkBox (WIP)
+## Displaying the link target easily
+
+With LinkBox you can easily display the target of your links in your webpages, by just including an extra class on them.
+Link the CSS stylesheet, the script and add the "linkbox-link" class to your links, the script will do the rest!
+
+### Example:
+
+```
+<link type="text/css" rel="stylesheet" href="res/linkbox.css"/>
+...
+<p>My text <a href="https://www.github.com" class="linkbox-link">here</a></p>
+...
+<script type="text/javascript" src="res/scripts/linkbox.js"></script>
+```
---
+++ src/linkbox.js
@@ -0,0 +1,69 @@
+(function(){
+var linkBox = function(options){
+	//Default options values
+	this.defaults = {
+			linkClass: "linkbox-link",
+			linkDescriptionClass: "linkbox-description"
+	};
+
+	//Ignore any extra options besides the ones defined in the defaults, but change the values
+	//to match the given options
+	this.applyOptions = function(options){
+		var returnedOptions = this.defaults;
+		for(var opt in this.defaults){
+			if(options.hasOwnProperty(opt)){
+				returnedOptions[opt] = options[opt];
+			}
+		}
+
+		return returnedOptions;
+	};
+
+	//Show the box with the link description
+	this.showDescription = function(e) {
+		//>>>LATER: Maybe it'd be better to just have a hidden <div> on body and display it when necessary instead of creating it everytime!
+		//Create the div element that holds the description
+		var description_box = document.createElement("DIV");
+		//>>>LATER: Bind the function so we can access the description box class in the function object and use it instead!
+		description_box.className += " linkbox-description";
+		var description_paragraph = document.createElement("P");
+
+		//Puts random text for testing
+		description_paragraph.appendChild(document.createTextNode(e.target.href));
+		description_box.appendChild(description_paragraph);
+		document.body.appendChild(description_box);
+
+		//Position element
+		var posRect = e.target.getBoundingClientRect();
+		description_box.style.top = (posRect.y + e.target.offsetHeight + 1) + "px";
+		description_box.style.left = (posRect.x - description_box.offsetWidth/2 + e.target.offsetWidth/2) + "px";
+
+		this.activeElement = description_box;
+	};
+
+	//Hide the box with the link description
+	this.hideDescription = function(e) {
+		if(this.activeElement){
+			this.activeElement.outerHTML = "";
+			this.activeElement = "";
+		}
+	};
+
+	//Search the document for link elements with the linkClass and set up a listener
+	this.setUpListeners = function(){
+		//>>>LATER: Requires a polyfill for older browsers
+		var el = document.getElementsByClassName(this.options.linkClass);
+		for(var i = 0; i < el.length; i++){
+			el[i].addEventListener('mouseover', this.showDescription, false);
+			el[i].addEventListener('mouseout', this.hideDescription, false);
+		}
+	};
+
+	this.options = this.applyOptions(options || {});
+	console.log(this.options);
+	this.activeElement = "";
+	this.setUpListeners();
+};
+
+linkBox();
+})();
