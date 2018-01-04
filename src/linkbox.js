e58a3f3576f6415ff5e9fa12461dbeaee92b22d0
IanCaio <iancaio_dev@hotmail.com>
2018-01-04 15:31:30 -0200

    Reuse the <div> element to display the links
    	Instead of creating a new element everytime the mouse hovers a link and deleting it after it's out, we are now using a single element to display the link description and just hiding it when it's not used.

--- src/linkbox.js
+++ src/linkbox.js
@@ -1,14 +1,14 @@
 (function(){
 var linkBox = function(options){
 	this.options = this.applyOptions(options || {});
-	this.activeElement = "";
+	this.boxElement = this.createBox(); //DOM reference to the description box element
 	this.setUpListeners();
 };
 
 //Default options values
 linkBox.prototype.defaults = {
 	linkClass: "linkbox-link",
-	linkDescriptionClass: "linkbox-description"
+	linkDescriptionID: "linkbox-description"
 };
 
 //Ignore any extra options besides the ones defined in the defaults, but change the values
@@ -24,34 +24,41 @@ linkBox.prototype.applyOptions = function(options){
 	return returnedOptions;
 };
 
-//Show the box with the link description
-linkBox.prototype.showDescription = function(e) {
-	//>>>LATER: Maybe it'd be better to just have a hidden <div> on body and display it when necessary instead of creating it everytime!
+//Create the box that will be used to show the link description
+linkBox.prototype.createBox = function() {
 	//Create the div element that holds the description
 	var description_box = document.createElement("DIV");
-	//>>>LATER: Bind the function so we can access the description box class in the function object and use it instead!
-	description_box.className += " " + this.options.linkDescriptionClass;
+	description_box.id = this.options.linkDescriptionID;
 	var description_paragraph = document.createElement("P");
 
-	//Puts random text for testing
-	description_paragraph.appendChild(document.createTextNode(e.target.href));
 	description_box.appendChild(description_paragraph);
 	document.body.appendChild(description_box);
 
+	return description_box;
+};
+
+//Show the box with the link description
+linkBox.prototype.showDescription = function(e) {
+	//Write link href
+	this.boxElement.getElementsByTagName("P")[0].innerHTML = e.target.href;
+
+	//Display element
+	//We need to display the element before calculating its position, otherwise it will consider the element doesn't have any width or height
+	this.boxElement.style.display = "block";
+
 	//Position element
 	var posRect = e.target.getBoundingClientRect();
-	description_box.style.top = (posRect.y + e.target.offsetHeight + 1) + "px";
-	description_box.style.left = (posRect.x - description_box.offsetWidth/2 + e.target.offsetWidth/2) + "px";
-
-	this.activeElement = description_box;
+	this.boxElement.style.top = (posRect.y + e.target.offsetHeight + 1) + "px";
+	this.boxElement.style.left = (posRect.x - this.boxElement.offsetWidth/2 + e.target.offsetWidth/2) + "px";
 };
 
 //Hide the box with the link description
 linkBox.prototype.hideDescription = function(e) {
-	if(this.activeElement){
-		this.activeElement.outerHTML = "";
-		this.activeElement = "";
-	}
+	//Delete link href
+	this.boxElement.getElementsByTagName("P")[0].innerHTML = "";
+
+	//Hide element
+	this.boxElement.style.display = "none";
 };
 
 //Search the document for link elements with the linkClass and set up a listener
